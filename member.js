ba300dbb958ae23c2dc8dbf9cdd4c2843760475b
albertovp17 <132883621+albertovp17@users.noreply.github.com>
2023-07-18 09:25:17 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+  var member = document.getElementById("member");
+  var skills = document.getElementById("skills");
+  var experience = document.getElementById("experience");
+  var projects = document.getElementById("projects");
+  var contact = document.getElementById("contact");
+
+  member.style.display = "block";
+  skills.style.display = "none";
+  experience.style.display = "none";
+  projects.style.display = "none";
+  contact.style.display = "none";
+}
